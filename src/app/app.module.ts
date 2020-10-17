f72012566ac12298867260f52a5dee8a75d86bd1
Alicia <alicia.minet75@laposte.net>
2020-10-17 03:03:27 +0200

    add swipe back gesture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,9 @@ import { HttpClientModule } from '@angular/common/http';
   entryComponents: [],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot({
+        swipeBackEnabled: true
+      }),
     AppRoutingModule,
     HttpClientModule
   ],
